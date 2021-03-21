4879dd70dc2d19192f47306be25d64f1db98a152
Konstantinos Kasidiaris <kkasidiaris@gmail.com>
2021-03-22 01:29:32 +0200

    added cors handling

--- comments/index.js
+++ comments/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const { randomBytes } = require('crypto');
 const app = express();
+const cors = require('cors');
 
 app.use(express.json());
+app.use(cors());
 
 const commentsByPostId = {};
 
