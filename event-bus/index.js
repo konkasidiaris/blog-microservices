e4914322cb92eeb2b8cee2203c4d6420b8de798e
Konstantinos <kkasidiaris@gmail.com>
2021-03-25 05:36:46 +0200

    client uses the query service now

--- event-bus/index.js
+++ event-bus/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 const axios = require("axios");
+const cors = require("cors");
 
 app.use(express.json());
+app.use(cors());
 
 app.post("/events", async (req, res) => {
   try {
